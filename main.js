df4cc24c7dc5aea09bf2cd34ee8c5d0b01266588
Sif29 <128655899+Sif29@users.noreply.github.com>
2023-06-07 20:48:44 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,23 @@
+var canvas = new fabric.Canvas('myCanvas');
+ var x= document.getElementById("myAudio");
+
+function new_image()
+{
+	
+	
+}
+
+function playSound(){
+	
+}
+
+#myCanvas
+{
+border-width:10px;
+background-color: white;
+border-Style:ridge
+border-color:darkviolet
+}
+
+<button class="btn btn-warning btn-lg"
+onclick="playSound"> HAPPY BIRTHDAY TO YOUUUUUUU</button>
